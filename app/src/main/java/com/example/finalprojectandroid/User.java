4b6f2b3a1c9b7b690724aa8c761581fb301cb837
mohammad-obeid-1200644 <mohammadkadoumi77@yahoo.com>
2024-01-03 01:41:28 +0200

    Version 1.0

---
+++ app/src/main/java/com/example/finalprojectandroid/User.java
@@ -0,0 +1,62 @@
+package com.example.finalprojectandroid;
+
+import java.util.Objects;
+
+public class User {
+    private String name,email,pass;
+
+    public User() {
+    }
+
+    public User(String name, String email, String pass) {
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name) && email.equals(user.email) && pass.equals(user.pass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, pass);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
+}
