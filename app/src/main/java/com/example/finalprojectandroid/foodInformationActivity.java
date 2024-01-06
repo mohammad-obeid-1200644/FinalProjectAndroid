ec39af97dfdb083de8d85597f95845b549c23af5
97059 <raghad.saleem.aqel@gmail.com>
2024-01-06 19:10:46 +0200

    v 4.0

--- app/src/main/java/com/example/finalprojectandroid/foodInformationActivity.java
+++ app/src/main/java/com/example/finalprojectandroid/foodInformationActivity.java
@@ -5,13 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class foodInformationActivity extends AppCompatActivity {
+
+    Button btnDecrement,btnIncrement;
+    TextView txtQuantity;
+    int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_information);
+        btnDecrement = findViewById(R.id.btnDecrement);
+        btnIncrement = findViewById(R.id.btnIncrement);
+        txtQuantity = findViewById(R.id.txtQuantity);
+    }
+
+    public void incrementOnClick(View view){
+        count++;
+        txtQuantity.setText(""+count);
+
+    }
+
+    public void decrementOnClick(View view){
+        if(count<=1)
+            count=1;
+        else
+            count--;
+        txtQuantity.setText(""+count);
     }
 
     public void backtolistclk(View view) {
