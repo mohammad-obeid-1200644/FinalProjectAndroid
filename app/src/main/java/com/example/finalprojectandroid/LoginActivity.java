4f1e37cae7fabcee518fed47b47ce38f06f0f51b
97059 <raghad.saleem.aqel@gmail.com>
2024-01-21 04:54:00 +0200

    v 8.0

--- app/src/main/java/com/example/finalprojectandroid/LoginActivity.java
+++ app/src/main/java/com/example/finalprojectandroid/LoginActivity.java
@@ -4,23 +4,108 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edtName, edtPassword;
+    CheckBox chkRememberMe;
+    private RequestQueue queue;
+    ArrayList<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setUpViews();
     }
 
-    public void signupclk(View view) {
+    public void setUpViews(){
+        queue = Volley.newRequestQueue(this);
+        edtName = findViewById(R.id.edtName);
+        edtPassword = findViewById(R.id.edtPassword);
+        chkRememberMe = findViewById(R.id.chkRememberMe);
+    }
+
+    public void signUpOnClk(View view) {
         Intent intent=new Intent(LoginActivity.this, SignupActivity.class);
         startActivity(intent);
     }
 
-    public void loginclk(View view) {
-        Intent intent=new Intent(LoginActivity.this, CafList.class);
-        startActivity(intent);
+    private boolean isEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+
+    public void logInOnClk(View view) {
+        String name = edtName.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        if (isEmpty(name) && isEmpty(password)){
+            Toast.makeText(LoginActivity.this, "Please Fill in all fields!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        String url = "http://10.0.2.2:5000/user";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,
+                null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                users = new ArrayList<>();
+               // boolean userFound = false;
+
+                if (response != null) {
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject obj = response.getJSONObject(i);
+                            String dataBaseName = obj.getString("Name");
+                            String dataBasePassword = obj.getString("Password");
+
+
+
+                            if (dataBaseName.equals(name) && dataBasePassword.equals(password)){
+                                Intent intent=new Intent(LoginActivity.this, CafList.class);
+                                startActivity(intent);
+                            }
+                        } catch (JSONException exception) {
+                            Log.d("Error", exception.toString());
+                        }
+                    }
+                    Toast.makeText(LoginActivity.this, "This user does not exists!", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(LoginActivity.this, "No Users in the database!", Toast.LENGTH_LONG).show();
+                    Log.d("Error_json", "Null response from the server.");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(LoginActivity.this, error.toString(),
+                        Toast.LENGTH_SHORT).show();
+                Log.d("Error_json", error.toString());
+            }
+        });
+
+        queue.add(request);
+
+
     }
 }
--- app/src/main/java/com/example/finalprojectandroid/SignupActivity.java
+++ app/src/main/java/com/example/finalprojectandroid/SignupActivity.java
@@ -3,12 +3,16 @@ package com.example.finalprojectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,17 +20,21 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 
 
 public class SignupActivity extends AppCompatActivity {
     private EditText edtName, edtEmail, edtPassword, edtRepeatPassword;
-    private Button btnSignUp;
+    private RequestQueue queue;
+    ArrayList<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,7 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void setUpViews(){
+        queue = Volley.newRequestQueue(this);
         edtName = findViewById(R.id.edtName);
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
@@ -47,8 +56,92 @@ public class SignupActivity extends AppCompatActivity {
         String userName = edtName.getText().toString();
         String userEmail = edtEmail.getText().toString();
         String userPassword = edtPassword.getText().toString();
+        String repeatedPassword = edtRepeatPassword.getText().toString();
 
-        addUser(userName, userEmail, userPassword);
+
+        if (!isEmpty(userName) && !isEmpty(userEmail) && !isEmpty(userPassword) && !isEmpty(repeatedPassword)) {
+            if (isValidEmail(userEmail) && userPassword.equals(repeatedPassword)) {
+                if (isValidPassword(userPassword)) {
+                    //read the user table from database to make sure that there is no two same users
+                    String url = "http://10.0.2.2:5000/user";
+                    JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,
+                            null, new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            users = new ArrayList<>();
+                            boolean userFound = false;
+
+                            if (response != null) {
+                                for (int i = 0; i < response.length(); i++) {
+                                    try {
+                                        JSONObject obj = response.getJSONObject(i);
+                                        User user = new User(
+                                                obj.getString("Name"),
+                                                obj.getString("Email"),
+                                                obj.getString("Password")
+                                        );
+                                        users.add(user);
+                                    } catch (JSONException exception) {
+                                        Log.d("Error", exception.toString());
+                                    }
+                                }
+
+                                for (int i = 0; i < users.size(); i++) {
+                                    User user = users.get(i);
+                                    User userToAdd = new User(userName, userEmail, userPassword);
+                                    if (user.equals(userToAdd)) {
+                                        userFound = true;
+                                        Toast.makeText(SignupActivity.this, "User Exists!", Toast.LENGTH_LONG).show();
+                                        break;
+                                    }
+                                }
+                            }else {
+                                Toast.makeText(SignupActivity.this, "No Users in the database!", Toast.LENGTH_LONG).show();
+                                Log.d("Error_json", "Null response from the server.");
+                            }
+
+                            if (!userFound) {
+                                addUser(userName, userEmail, userPassword);
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            Toast.makeText(SignupActivity.this, error.toString(),
+                                    Toast.LENGTH_SHORT).show();
+                            Log.d("Error_json", error.toString());
+                        }
+                    });
+
+                    queue.add(request);
+
+                } else {
+                    Toast.makeText(SignupActivity.this, "Password must be at least 8 characters long, including numbers, and contain at least one character.", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                if (!isValidEmail(userEmail)) {
+                    Toast.makeText(SignupActivity.this, "Invalid email address.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SignupActivity.this, "Passwords do not match.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }else {
+            Toast.makeText(SignupActivity.this, "Please fill in all fields.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isValidPassword(String password) {
+        String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+        return password.matches(passwordPattern);
+    }
+
+    private boolean isValidEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.trim().isEmpty();
     }
 
     private void addUser(String userName, String userEmail, String userPassword){
@@ -93,4 +186,25 @@ public class SignupActivity extends AppCompatActivity {
         );
         queue.add(request);
     }
+
+//    private boolean isValidPassword(String password) {
+//        if (password.length() < 8) {
+//            return false;
+//        }
+//        boolean containsAlphabetic = false;
+//        for (char c : password.toCharArray()) {
+//            if (Character.isLetter(c)) {
+//                containsAlphabetic = true;
+//                break;
+//            }
+//        }
+//        boolean containsDigit = false;
+//        for (char c : password.toCharArray()) {
+//            if (Character.isDigit(c)) {
+//                containsDigit = true;
+//                break;
+//            }
+//        }
+//        return containsAlphabetic && containsDigit;
+//    }
 }
